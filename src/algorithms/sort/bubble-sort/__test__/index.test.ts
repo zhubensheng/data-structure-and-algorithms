42d1fc75aef02850676b3a425c1e504d7cb9bf75
1029174296@qq.com <apl9922@aplun.net>
2020-12-02 15:25:31 +0800

    新增冒泡排序

--- src/algorithms/sort/bubble-sort/__test__/index.test.ts
+++ src/algorithms/sort/bubble-sort/__test__/index.test.ts
@@ -1,5 +1,5 @@
 import bubbleSort from '../index';
 
 test('bubble sort test', () => {
-  expect(bubbleSort([2, 1, 4, 5, 2, 20932, 1])).toBe([1, 1, 2, 2, 4, 5, 20932]);
+  expect(bubbleSort([2, 1, 4, 5, 2, 20932, 1])).toEqual([1, 1, 2, 2, 4, 5, 20932]);
 });
--- src/algorithms/sort/bubble-sort/index.ts
+++ src/algorithms/sort/bubble-sort/index.ts
@@ -5,7 +5,7 @@
 export default function bubbleSort<T>(xs: T[]): T[] {
   const n = xs.length;
   for (let i = 0; i < n; i++) {
-    for (let j = i; j < n - i - 1; j++) {
+    for (let j = 0; j < n - i - 1; j++) {
       if (xs[j] > xs[j + 1]) {
         // swap
         const temp = xs[j];
